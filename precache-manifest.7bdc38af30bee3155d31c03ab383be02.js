953b0a1d5afcc5e0c8b4ef07214cb90c78ecbc1a
adberg2001 <aaaadyl.erg@gmail.com>
2020-10-28 05:56:23 +0600

    Updates

---
+++ precache-manifest.7bdc38af30bee3155d31c03ab383be02.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d191003507d209b7c470ac3cfa6849c9",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "6c537da87efb3fa901d2",
+    "url": "/resume/static/css/main.431e0ae7.chunk.css"
+  },
+  {
+    "revision": "1d158f43bf49b02ddef4",
+    "url": "/resume/static/js/2.a25b3666.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/resume/static/js/2.a25b3666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c537da87efb3fa901d2",
+    "url": "/resume/static/js/main.3e4b9e0a.chunk.js"
+  },
+  {
+    "revision": "f9af19f1647f58f73110",
+    "url": "/resume/static/js/runtime-main.39af25b7.js"
+  },
+  {
+    "revision": "88847fcdc79ebeec6dd67d10db63901d",
+    "url": "/resume/static/media/Facebook_Logo.88847fcd.png"
+  },
+  {
+    "revision": "be2c0b6d342ffcc716341091b7497160",
+    "url": "/resume/static/media/Gmail_Icon.be2c0b6d.png"
+  },
+  {
+    "revision": "5e7b7fb126593fe9cbaed6eed888bc77",
+    "url": "/resume/static/media/Instagram-icon-WHITE.5e7b7fb1.png"
+  },
+  {
+    "revision": "c938bf07d8d4c7b9db326c113c97a7d6",
+    "url": "/resume/static/media/Telegram.c938bf07.png"
+  },
+  {
+    "revision": "90c4441d9117b046285ea72341dd3aec",
+    "url": "/resume/static/media/adsad.90c4441d.png"
+  },
+  {
+    "revision": "ca8fb687ab2829c1cd6cd4aa8713e715",
+    "url": "/resume/static/media/bootstrap.ca8fb687.png"
+  },
+  {
+    "revision": "ffaff6369e4d1a48103f8eaf7567f5b5",
+    "url": "/resume/static/media/css.ffaff636.png"
+  },
+  {
+    "revision": "edf9a3bc78615eedd4873d82df00ae3a",
+    "url": "/resume/static/media/figma.edf9a3bc.png"
+  },
+  {
+    "revision": "e3bd5c906386fff3d91234a96d1358c5",
+    "url": "/resume/static/media/github_PNG.e3bd5c90.png"
+  },
+  {
+    "revision": "165565e7ede57410eb8b4f04482c07d6",
+    "url": "/resume/static/media/instagram.165565e7.png"
+  },
+  {
+    "revision": "0df82260dfb30aeab9075764c885a4a1",
+    "url": "/resume/static/media/json.0df82260.png"
+  },
+  {
+    "revision": "57ed21a2cc3576a1d41cbdbc9d16eb94",
+    "url": "/resume/static/media/linkedIn_PNG38.57ed21a2.png"
+  },
+  {
+    "revision": "7a48d808962dcd2cf273c55d9c56ce87",
+    "url": "/resume/static/media/logoTelegram.7a48d808.png"
+  },
+  {
+    "revision": "09e0f887d8991f3ae3afb15f2f6168e2",
+    "url": "/resume/static/media/oxford.09e0f887.png"
+  },
+  {
+    "revision": "af3b083d6468ce9b6934597f0fa9b062",
+    "url": "/resume/static/media/postman.af3b083d.png"
+  },
+  {
+    "revision": "85ca750b7711ededbec61d5488b4263e",
+    "url": "/resume/static/media/react-icon-png-7.85ca750b.png"
+  },
+  {
+    "revision": "96e5850daed65a300d49b5f1acc9f120",
+    "url": "/resume/static/media/vue.96e5850d.png"
+  },
+  {
+    "revision": "232b5c9576505585e5c9c8cb92a2fd82",
+    "url": "/resume/static/media/whatsapp.232b5c95.png"
+  }
+]);
